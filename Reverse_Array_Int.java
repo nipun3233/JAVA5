efb64cfa28a273f05661b5c57523589713a16578
NIPUN SINHA <nipunsinha97@gmail.com>
2021-09-23 23:22:55 +0530

    sixth  commit

---
+++ Reverse_Array_Int.java
@@ -0,0 +1,11 @@
+package home;
+
+public class Reverse_Array_Int {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5};
+		int rey[] = {};
+		for (int i=0; i<5; i++) {
+			rey[i]=arr[5-i];
+		}
+	}
+}
