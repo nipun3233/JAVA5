f67497b6bc6d5763c395b01f95cb1bf20b17c7a9
NIPUN SINHA <nipunsinha97@gmail.com>
2021-10-04 23:09:15 +0530

    first 9.1  commit

--- Even_sorter.java
+++ Even_sorter.java
@@ -1,13 +1,17 @@
 package home;
 
 public class Even_sorter {
+
+	
 	public static void main(String[] args) {
-		int[] num = {12,3,4,5,10,7,8,13};
-		int [] result = new int[num.length];
+		int[] num = {1,2,3,4,5,6,7,8,9,10};
+		int [] result = new int[10];
 		int j=1;
+		int k=0;
 		for(int i=0;i<num.length;i++) {
 			if(num[i]%2==0) {
-				result[i]=num[i];
+				result[k]=num[i];
+				k++;
 			}
 			else {
 				result[num.length-j]=num[i];
@@ -18,4 +22,7 @@ public class Even_sorter {
 			System.out.print(result[i]);
 		}
 }
+	
+	
+	
 }
