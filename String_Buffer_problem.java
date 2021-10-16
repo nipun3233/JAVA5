5aa66cb188f331c15fb585ea111c2d3b7faf727f
NIPUN SINHA <nipunsinha97@gmail.com>
2021-10-16 23:41:38 +0530

    tenth1 commit

---
+++ String_Buffer_problem.java
@@ -0,0 +1,18 @@
+package home;
+
+public class String_Buffer_problem {
+	public static void main(String args[]) {
+        
+	      
+	      String Str = new String("Welcome to Geekster");
+	        
+	     
+	      System.out.print("After replacing all o with T : " );
+	      System.out.println(Str.replace('o', 'T'));
+	        
+	      
+	      System.out.print("After replacing all e with D : " );
+	      System.out.println(Str.replace('e', 'D'));
+	        
+	   }
+}
