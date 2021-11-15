93366bffe0ee6721baaa200776e47a54340d23f0
NIPUN SINHA <nipunsinha97@gmail.com>
2021-11-15 23:31:03 +0530

    twelvth commit

---
+++ callby.java
@@ -0,0 +1,20 @@
+package home;
+
+public class callby {
+
+		 int data=50;  
+		  
+		 void change(int data){  
+		 data=data+100;//changes will be in the local variable only  
+		 }  
+		     
+		 public static void main(String args[]){  
+			 callby op=new callby();  
+		  
+		   System.out.println("before change "+op.data);  
+		   op.change(500);  
+		   System.out.println("after change "+op.data);  
+		  
+		 }  
+		}  
+
