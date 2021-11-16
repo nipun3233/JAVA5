3fbbd7caaeadb8f975c338e1614735fd9bb3bef8
NIPUN SINHA <nipunsinha97@gmail.com>
2021-11-17 03:20:53 +0530

    completed

--- binarySearch.java
+++ binarySearch.java
@@ -1,25 +1,34 @@
 package Advance_DSA;
 
 public class binarySearch {
+	
 	static int binary_search(int[] arr,int target) {
 	int low=0, high=arr.length-1;	
+	
 	while(low<=high) {
-		int mid = (low=high)/2;
+		
+		int mid = low+(high-low)/2;
+		
 		if(arr[mid]==target)
 			return mid;
-		if(target < arr[mid]) {
-			high=mid-1;
+		
+		else if(target < arr[mid]) {
+			high = mid-1;
 		}
 		else {
-			low=mid+1;
+			low = mid+1;
 		}
 	}
-	return low;
+	//if (low==arr.length || high<0) return -1;
+	
+	return -1;
 	}
+	
 	
 	public static void main(String [] args) {
 		int arr[]= {2,3,4,5,7,8,10,11,15};
-		int target = 2;
+		int target = 9;
+				;
 		System.out.println("the no is "+binary_search(arr,target));
 	}
 }
