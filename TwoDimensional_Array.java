ed3afd901aa563cc7410c6b86334e6cd25ce2a6c
NIPUN SINHA <nipunsinha97@gmail.com>
2021-11-21 00:11:12 +0530

    Adv DSA

--- TwoDimensional_Array.java
+++ TwoDimensional_Array.java
@@ -16,6 +16,30 @@ public class TwoDimensional_Array {
 		for(int i=0;i<n;i++) {
 			System.out.print(arr[i]);
 		}
+		
+		System.out.println("\n enter the size of rows and column of your matrix");
+		int r =sc.nextInt();
+		int k =0;
+		int c = sc.nextInt();
+		int[][] mat= new int[r][c];  
+
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<c;j++) {
+				mat[i][j]=arr[k];
+				k++;
+			}
+		}
+		
+		
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<c;j++) {
+				System.out.print(mat[i][j]);
+			}
+			System.out.println("");
+		}
+		
+		
+		
 	sc.close();			
 	}
 }
