fff4216d8c1ee796cc7943356b785f1e14ad7ae0
NIPUN SINHA <nipunsinha97@gmail.com>
2021-11-21 18:39:04 +0530

    ADV DSA

--- TwoDimensional_Array.java
+++ TwoDimensional_Array.java
@@ -1,54 +1,58 @@
-package LeetCode;
+package Advance_DSA;
 import java.util.*;
-public class TwoDimensional_Array {
-	public static int[][] construct2DArray(int[] arr, int r ,int c){
-		int[][] mat= new int[r][c];  
-		int n = arr.length;
-		int k=0;
-//		if(arr.length != 0) {
-			if(r*c==n) {
-				for(int i=0;i<r;i++) {
-					for(int j=0;j<c;j++) {
-						mat[i][j]=arr[k];
-						k++;
-					}
-				}	
-			}
-			
-//			}
-			
-			
-			for(int i=0;i<r;i++) {
-				for(int j=0;j<c;j++) {
-					System.out.print(mat[i][j]);
-				}
-				System.out.println("");
-			}
-			
-		return null;
-		
-	}
-	public static void main(String[] args) {
+public class twodimensional_Array {
+	public static void main(String [] args) {
+		int a[][] = new int[3][3];
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of the Array");
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		System.out.println("proceed with the values,");
-		for (int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
+		System.out.println("enter first matrix");
+		for(int i = 0;i<3;i++) {
+				for(int j=0;j<3;j++) {
+						a[i][j]=sc.nextInt();
+				}
 		}
-		for(int i=0;i<n;i++) {
-			System.out.print(arr[i]);
+		System.out.println("enter second matrix");
+		int b[][] = new int[3][3];
+		for(int i = 0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+					b[i][j]=sc.nextInt();
+			}
 		}
 		
-		System.out.println("\nenter the size of rows and column of your matrix");
-		int r =sc.nextInt();
-		int c = sc.nextInt();
 		
+//		System.out.println("matrix product");
+		int c[][] = new int[3][3];
+				for(int i=0;i<3;i++) {
+					for(int j=0;j<3;j++) {
+						c[i][j]=0;
+						for(int k=0;k<3;k++) {
+							c[i][j]+=a[i][k] * b[k][j];
+						}
+					}
+				}
 		
-		construct2DArray(arr , r,c);
+		for(int i = 0;i<3;i++) {
+					for(int j=0;j<3;j++) {
+							System.out.print(" "+a[i][j]);
+					}
+					System.out.println("");
+			}
+		System.out.println("");
 		
-		
-	sc.close();			
+		for(int i = 0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+					System.out.print(" "+b[i][j]);
+			}
+			System.out.println("");
+			}
+				
+		System.out.println("");			
+				
+		for(int i = 0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+					System.out.print(" "+c[i][j]);
+			}
+			System.out.println("");
+	}
+		sc.close();
 	}
 }
